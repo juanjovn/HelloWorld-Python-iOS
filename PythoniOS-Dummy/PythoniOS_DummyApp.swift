79505cea8cfd58854f0a68646c750de5885c5cc1
Juanjo Valiño <juanjovn@gmail.com>
2022-09-08 14:20:20 +0200

    Initial Commit

---
+++ PythoniOS-Dummy/PythoniOS_DummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PythoniOS_DummyApp.swift
+//  PythoniOS-Dummy
+//
+//  Created by Juanjo Valiño on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PythoniOS_DummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
